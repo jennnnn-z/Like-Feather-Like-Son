3a45c91b7bc0d467fe7361f1925ebd0b04c442d5
kechezon <115599689+kechezon@users.noreply.github.com>
2022-11-09 20:08:22 -0500

    Implemented Flight and Glide*
    Will need Layer Trigger for landing.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 4;
+    private float jumpForce = 6;
+    private float grav = 1f;
     private float acceleration;
     private Rigidbody2D playerRb;
 
     public float horizontalInput;
     public float verticalInput;
+    public KeyCode jump = KeyCode.Space;
+
+    public MoveState playerMove;
+    public AirState playerAir;
+
+    private float jumpTime;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
         acceleration = speed * 2;
+        playerMove = MoveState.Stationary;
+        playerAir = AirState.Grounded;
+        jumpTime = 0;
     }
 
     // Update is called once per frame
@@ -24,16 +35,74 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         float zDegrees = Vector2.SignedAngle(new Vector2(0, 1), new Vector2(horizontalInput, verticalInput));
-        Debug.Log(zDegrees);
 
         if (Mathf.Abs(verticalInput) >= 0.5f || Mathf.Abs(horizontalInput) >= 0.5f)
         {
-            transform.eulerAngles = new Vector3(0, 0, zDegrees);
-            playerRb.velocity = transform.up*speed;
+            if (playerMove == MoveState.Stationary)
+            {
+                transform.eulerAngles = new Vector3(0, 0, zDegrees);
+                playerRb.velocity += new Vector2(transform.up.x, transform.up.y).normalized * speed;
+                playerMove = MoveState.Moving;
+            }
         }
         else
         {
-            playerRb.velocity *= 0;
+            if (playerMove == MoveState.Moving)
+            {
+                playerRb.velocity -= new Vector2(transform.up.x, transform.up.y).normalized*speed;
+                playerMove = MoveState.Stationary;
+            }
+        }
+
+        if (playerAir == AirState.Grounded)
+        {
+            playerRb.gravityScale = 0;
+            if (Input.GetKeyDown(jump))
+            {
+                playerRb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+                playerRb.gravityScale = grav;
+                playerAir = AirState.Rising;
+                //Change Layer
+            }
+        }
+        else if (playerAir == AirState.Rising)
+        {
+            jumpTime += Time.deltaTime;
+            Debug.Log(jumpTime);
+            if (jumpTime > 1)
+            {
+                jumpTime = 0;
+                playerAir = AirState.Airborne;
+            }
+        }
+        else if (playerAir == AirState.Airborne || playerAir == AirState.Gliding)
+        {
+            if (Input.GetKey(jump))
+            {
+                playerRb.gravityScale = grav/2;
+                playerAir = AirState.Gliding;
+            }
+            else
+            {
+                playerRb.gravityScale = grav;
+                playerAir = AirState.Airborne;
+            }
         }
     }
+
+    public enum MoveState
+    {
+        Stationary,
+        Moving
+    }
+
+    public enum AirState
+    {
+        Grounded,
+        Rising,
+        Airborne,
+        Gliding
+    }
+
+    //Layer Trigger should also set state to grounded.
 }
